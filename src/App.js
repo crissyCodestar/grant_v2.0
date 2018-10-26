18182297be3cb1d5fd51e646831d144862fa260c
Crissy <crystalgrant82@gmail.comgit config --global user.email crystalgrant82@gmail.com>
2018-10-25 23:21:35 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import SocialLinks from './Components/SocialLinks'
+// import LandingPage from './Pages/LandingPage';
+// import TechPlayground from './Pages/TechPlayground';
+// import ArtGallery from './Pages/ArtGallery';
+// import About from './Pages/About';
+// import Contact from './Pages/Contact';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Crystals Portfolio </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
