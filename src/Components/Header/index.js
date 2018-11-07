fbafe28dcf7e267bd1b57221328f336d877d5eca
Crissy <crystalgrant82@gmail.comgit config --global user.email crystalgrant82@gmail.com>
2018-11-06 20:04:22 -0500

    Tested css transitons for header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,12 +3,22 @@ import farBg from '../../../images/far_bg.png';
 import forBg from '../../../images/for_bg_two.png';
 import './Header.css';
 
-const Header = () => (
-  <div className='bg_container'>
-    <div className='far_bg'>
-        <p>...grant</p>
-    </div>
 
+const Header = () => (
+  <div className='main_container'>
+    <div className='bg_container'>
+      <div className='far_bg_cris'>
+          <p className='cris_p'>crystal...</p>
+          <button className='bn'></button>
+      </div>
+
+    </div>
+    <div className='bg_container'>
+      <div className='far_bg'>
+          <p className='grant_p'>...grant</p>
+      </div>
+
+    </div>
   </div>
 )
 
